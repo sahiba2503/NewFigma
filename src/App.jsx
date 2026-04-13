c7741b5a003e2e274e18afd706ffa3b3c30531d2
sahiba <25sahiba03@gmail.com>
2026-04-14 00:21:12 +0530

    add welcome section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from './Navbar' 
 import './App.css'
+import Welcome from './Welcome'
 function App() {
   return (
     <div>
       <Navbar />
+      <Welcome />
     </div> 
   )
 }
---
+++ src/Welcome.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Welcome() {
+  return (
+    <div>
+      <div className='Main-image'></div>
+      <div className='welcomTo'>
+        <span>Welcome To</span>
+        <h1>
+        Where innovation meets tomorrow</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo nisi ab cumque officiis perspiciatis, unde animi quisquam itaque quo ad similique distinctio natus, eius quod maiores doloremque debitis reprehenderit veritatis.</p>
+        <button>Primary</button>
+        <button>Secondary</button>
+        </div>
+    </div>
+  )
+}
+
+export default Welcome
