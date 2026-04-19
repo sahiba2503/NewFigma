4af143de7163ef8d6be8b964b3f0ce5caa096f0f
sahiba <25sahiba03@gmail.com>
2026-04-20 00:08:40 +0530

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
-import Navbar from './Navbar' 
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import About from "./About";
+import Blog from "./Blog";
+import Contact from "./Contact"; 
 import './App.css'
 import Welcome from './Welcome'
 import Boxes from './Boxes'
@@ -9,19 +15,30 @@ import LatestArticle from './LatestArticle'
 import SubscribeNew from './SubscribeNew'
 import Footer from './Footer'
 
+
+
 function App() {
   return (
-    <div>
-      <Navbar />
+      <BrowserRouter>
+         <Navbar />
+
       <Welcome />
       <Boxes />
-    <StrategySec />
+     <StrategySec />
     <Technology />
      <GroundBreaking />
-    <LatestArticle />
-    <SubscribeNew />
-    <Footer />
-    </div> 
+     <LatestArticle />
+     <SubscribeNew />
+     <Footer />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Blog" element={<Blog />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+    
   )
 }
 
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,28 +1,36 @@
+
 import React from 'react'
 import logo from "/Logo.svg";
 import square from "/Group 1670.svg";
 import lines from "/Icons (1).svg";
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
     <nav>
       <div className='navList'>
-        <img src={lines} alt=""  className='threeLine'/>
-    <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Blog</li>
-        <li>Contact</li>
-           </ul>
-           </div>
-           <div className='logo-btn'>
-           <div>
-            <img src={logo} alt="logo"  className='rectangularLogo'/>
-             <img src={square} alt="logo"  className='squareLogo'/>
-           </div>
-           <button style={{background:"black",color:"white"}}>Download Now</button>
-           </div>
-           </nav>
+        <img src={lines} alt="" className='threeLine' />
+
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/blog">Blog</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </div>
+
+      <div className='logo-btn'>
+        <div>
+          <img src={logo} alt="logo" className='rectangularLogo' />
+          <img src={square} alt="logo" className='squareLogo' />
+        </div>
+
+        <button style={{ background: "black", color: "white" }}>
+          Download Now
+        </button>
+      </div>
+    </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
