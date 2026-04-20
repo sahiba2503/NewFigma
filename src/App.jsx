17e2adbe21b0760b93afd4214bffea5968b87237
sahiba <25sahiba03@gmail.com>
2026-04-20 18:05:29 +0530

    add home page

--- src/App.jsx
+++ src/App.jsx
@@ -6,31 +6,10 @@ import About from "./About";
 import Blog from "./Blog";
 import Contact from "./Contact"; 
 import './App.css'
-import Welcome from './Welcome'
-import Boxes from './Boxes'
-import StrategySec from './StrategySec'
-import Technology from './Technology'
-import GroundBreaking from './GroundBreaking'
-import LatestArticle from './LatestArticle'
-import SubscribeNew from './SubscribeNew'
-import Footer from './Footer'
-
-
-
 function App() {
   return (
       <BrowserRouter>
          <Navbar />
-
-      <Welcome />
-      <Boxes />
-     <StrategySec />
-    <Technology />
-     <GroundBreaking />
-     <LatestArticle />
-     <SubscribeNew />
-     <Footer />
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/About" element={<About />} />
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Welcome from './Welcome'
+import Boxes from './Boxes'
+import StrategySec from './StrategySec'
+import Technology from './Technology'
+import GroundBreaking from './GroundBreaking'
+import LatestArticle from './LatestArticle'
+import SubscribeNew from './SubscribeNew'
+import Footer from './Footer'
 
 function Home() {
   return (
     <div>
+      <Welcome />
+      <Boxes />
+     <StrategySec />
+    <Technology />
+     <GroundBreaking />
+     <LatestArticle />
+     <SubscribeNew />
+     <Footer />
         </div>
   )
 }
