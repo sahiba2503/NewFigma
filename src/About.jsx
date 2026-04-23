62d8fcb7d02127217cc019e236c9f1a81cee86c9
sahiba <25sahiba03@gmail.com>
2026-04-23 21:52:14 +0530

    add layout about page

--- src/About.jsx
+++ src/About.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import AboutUsKvasition from './AboutUsKvasition'
 import AboutOurStory from './AboutOurStory'
+import AboutOur from './AboutOur'
 function About() {
   return (
        <div>
         <AboutUsKvasition/>
         <AboutOurStory />
+        <AboutOur />
        </div>
   )
 }
---
+++ src/AboutOur.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+function AboutOur() {
+  return (
+    <div className='aboutOurTeam'>
+       <div className="ourTeamHeading">
+        <h2>Our team has a over 100 years of experience combined</h2>
+        </div>
+       <div className="ourTeamBranches">
+        <div className="ourTeambox">
+            <div className="branchephoto"></div>
+            <h4>First Last</h4>
+            <strong>job title/Role/position</strong>
+            <button> Contact Name</button>
+        </div>
+        <div className="ourTeambox">
+              <div className="branchephoto"></div>
+                          <h4>First Last</h4>
+            <strong>job title/Role/position</strong>
+            <button> Contact Name</button>
+        </div>
+        <div className="ourTeambox">
+              <div className="branchephoto"></div>
+                          <h4>First Last</h4>
+            <strong>job title/Role/position</strong>
+            <button> Contact Name</button>
+        </div>
+        <div className="ourTeambox">
+              <div className="branchephoto"></div>
+                          <h4>First Last</h4>
+            <strong>job title/Role/position</strong>
+            <button> Contact Name</button>
+        </div>
+       </div>
+    </div>
+  )
+}
+
+export default AboutOur
