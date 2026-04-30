d656b387cb4de1b14ff1aa83f3ce2c279f8946ef
sahiba <25sahiba03@gmail.com>
2026-04-30 22:35:15 +0530

    add Blog page

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import BlogBody from './BlogBody'
+import BlogArchive from './BlogArchive'
+import Footer from "./Footer"
 
 function Blog() {
   return (
     <div className='BlogContainer'>
       <BlogBody />
+      <BlogArchive />
+      <Footer />
     </div>
   )
 }
---
+++ src/BlogArchive.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import icons from "/Icons (2).svg"
+import secicon from "/Icons (3).svg"
+
+function BlogArchive() {
+  return (
+    <div className='blogArchiveHeading'>
+        <hr></hr>
+        <h1>Archive</h1>
+        <div className="inputSection">
+            <div className='inputContainer'>
+            <img src={icons} alt="My Image" />
+            <input type="text" placeholder='Type Something' />
+             <img src={secicon} alt="My Image" />
+            </div>
+        </div>
+      <div className="archiveSection">
+        <div className="archiveFirstIreo">
+            <div className='archiveImg'></div>
+            <strong>Ireo</strong>
+            <div className='twoButton'><button>Chip</button><button>Chip</button></div>
+        </div>
+         <div className="archiveFirstIreo">
+            <div className='archiveImg'></div>
+            <strong>Ireo</strong>
+            <div className='twoButton'><button>Chip</button><button>Chip</button></div>
+        </div>
+         <div className="archiveFirstIreo">
+            <div className='archiveImg'></div>
+            <strong>Ireo</strong>
+            <div className='twoButton'><button>Chip</button><button>Chip</button></div>
+        </div>
+      </div>
+       <div className="archiveSection">
+        <div className="archiveFirstIreo">
+            <div className='archiveImg'></div>
+            <strong>Ireo</strong>
+            <div className='twoButton'><button>Chip</button><button>Chip</button></div>
+        </div>
+         <div className="archiveFirstIreo">
+            <div className='archiveImg'></div>
+            <strong>Ireo</strong>
+            <div className='twoButton'><button>Chip</button><button>Chip</button></div>
+        </div>
+         <div className="archiveFirstIreo">
+            <div className='archiveImg'></div>
+            <strong>Ireo</strong>
+            <div className='twoButton'><button>Chip</button><button>Chip</button></div>
+        </div>
+      </div>
+       <div className="archiveSection">
+        <div className="archiveFirstIreo">
+            <div className='archiveImg'></div>
+            <strong>Ireo</strong>
+            <div className='twoButton'><button>Chip</button><button>Chip</button></div>
+        </div>
+         <div className="archiveFirstIreo">
+            <div className='archiveImg'></div>
+            <strong>Ireo</strong>
+            <div className='twoButton'><button>Chip</button><button>Chip</button></div>
+        </div>
+         <div className="archiveFirstIreo">
+            <div className='archiveImg'></div>
+            <strong>Ireo</strong>
+            <div className='twoButton'><button>Chip</button><button>Chip</button></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default BlogArchive
