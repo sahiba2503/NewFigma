b66b676151f6200f732bf3e61e5fa7f40f7aa5fb
sahiba <25sahiba03@gmail.com>
2026-05-06 21:50:45 +0530

    add form

---
+++ src/ContactUsform.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ContactUsform() {
+  return (
+    <div>
+      <Navbar />
+
+    </div>
+  )
+}
+
+export default ContactUsform
---
+++ src/FormContact.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function FormContact() {
+  return (
+    <div className='inputSection'>
+        <div></div>
+        <div>
+            <h2>Contact us</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum praesentium iusto at, dolorum obcaecati impedit pariatur sapiente ipsa doloremque quibusdam fugiat quas commodi? Quidem aut obcaecati fugit dolorum impedit quas.</p>
+            <input type="text" />
+            <input type="text" />
+            <textarea name="" id=""></textarea>
+            <input type="checkbox"  />
+            <button className='Send message'>Send message</button>
+        </div>
+      
+    </div>
+  )
+}
+
+export default FormContact
