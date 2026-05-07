4ddfd698628cb8124ae98ccde5e2ed09310a6a50
sahiba <25sahiba03@gmail.com>
2026-05-07 22:20:19 +0530

    add form

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import ContactGustav from "./ContactGustav";
 import ContactFirstLast from "./ContactFirstLast";
 import ContactMoreLikeThis from "./ContactMoreLikeThis";
-
+import FormContact from "./FormContact"
 function Contact() {
   return (
     <div>      
       < ContactGustav />
       < ContactFirstLast />
       < ContactMoreLikeThis />
-     
+     <FormContact />
     </div>
   )
 }
--- src/FormContact.jsx
+++ src/FormContact.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 function FormContact() {
   return (
-    <div className='inputSection'>
-        <div></div>
-        <div>
-            <h2>Contact us</h2>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum praesentium iusto at, dolorum obcaecati impedit pariatur sapiente ipsa doloremque quibusdam fugiat quas commodi? Quidem aut obcaecati fugit dolorum impedit quas.</p>
-            <input type="text" />
-            <input type="text" />
-            <textarea name="" id=""></textarea>
+    <div className='formInputSection'>
+        <div className='contactFormImg'></div>
+        <div className='contactformSection'>
+            <h1>Contact us</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.as commodi? Quidem aut obcaecati fugit dolorum impedit quas.</p>
+            <input type="text" placeholder='Full Name'/>
+            <input type="text" placeholder='Email Address'/>
+            <textarea name="" id="" placeholder='Message'></textarea>
             <input type="checkbox"  />
             <button className='Send message'>Send message</button>
         </div>
