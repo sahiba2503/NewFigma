e8f51188bce213a089514598451eb6e1be4c8036
sahiba <25sahiba03@gmail.com>
2026-05-11 22:32:54 +0530

    add css to blog page for the big screen

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import BlogBody from './BlogBody'
 import BlogArchive from './BlogArchive'
 import Footer from "./Footer"
+import AboutFooter from './AboutFooter'
 
 function Blog() {
   return (
+    <>
     <div className='BlogContainer'>
       <BlogBody />
       <BlogArchive />
          </div>
+         <AboutFooter />
+         </>
   )
 }
 
