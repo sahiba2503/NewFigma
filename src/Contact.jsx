b856707530330bac194807332b134a164bc47209
sahiba <25sahiba03@gmail.com>
2026-05-15 20:15:08 +0530

    make responsive

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -3,16 +3,18 @@ import ContactGustav from "./ContactGustav";
 import ContactFirstLast from "./ContactFirstLast";
 import ContactMoreLikeThis from "./ContactMoreLikeThis";
 import FormContact from "./FormContact"
+import AboutFooter from './AboutFooter';
 function Contact() {
   return (
-    
+    <>
     <div className='containerContact'>      
       < ContactGustav />
       < ContactFirstLast />
       < ContactMoreLikeThis />
      <FormContact />
     </div>
-  
+    <AboutFooter />
+  </>
   )
 }
 
