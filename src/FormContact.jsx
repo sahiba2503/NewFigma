e879944d31d85f98c7c66f795669fdeb974fd4b6
sahiba <25sahiba03@gmail.com>
2026-05-17 21:09:43 +0530

    add form input container

--- src/FormContact.jsx
+++ src/FormContact.jsx
@@ -1,3 +1,4 @@
+import logo from "/Icons (4).svg"
 import React from 'react'
 
 function FormContact() {
@@ -7,8 +8,14 @@ function FormContact() {
         <div className='contactformSection'>
             <h1>Contact us</h1>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.as commodi? Quidem aut obcaecati fugit dolorum impedit quas.</p>
+            <div className="inputcontainer">
+              <img src={logo} alt="" />
             <input type="text" placeholder='Full Name'/>
+            </div>
+            <div className="inputcontainer">
+               <img src={logo} alt="" />
             <input type="text" placeholder='Email Address'/>
+            </div>
             <textarea name="" id="" placeholder='Message'></textarea>
             <input type="checkbox"  />
             <button className='Sendmessage'>Send message</button>
